4f959c63464906c409ed980cf2286bd6eb566671
chingliu1 <chingliu12@gmail.com>
2018-04-18 09:41:01 +0300

    designed the clear button for input deletion and it works

--- calc.js
+++ calc.js
@@ -1,3 +1,14 @@
 document.querySelector('form').addEventListener('submit', function(e){
     e.preventDefault();
-})
+});
+
+var delButton = document.getElementById('clear');
+var postMe = document.getElementById('input');
+var postMe2 = document.getElementById('output');
+
+delButton.addEventListener('click', function(e){
+    //console.log("am clicked");
+    
+    postMe.defaultValue = "";
+    postMe2.defaultValue = "";
+});
